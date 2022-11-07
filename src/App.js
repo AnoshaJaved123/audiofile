42a891ba81aff1d3014cfff125aea627ecd3285b
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-11-07 18:59:50 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+/*After much pain I discovered this codepen (https://codepen.io/naicuoctavian/pen/GRgzqBg)
+which simply and beutifully recorded audio in the browser with the Web Audio API, and 
+without third party libraries or issues.
+I have added a few features and imporved the UX to create what should be a nice starting
+point for implementing audio recording in your Reactjs App.
+*/
 
-function App() {
+import React, { useContext } from "react";
+import { MainContext } from "./contexts/MainContext.js";
+import AudioRecorder from "./AudioRecorder.js";
+import AudioRecorderFunc from "./AudioRecorderFunc.js";
+import SimpleRecorder from "./SimpleRecorder.js";
+import WaveSurferComponent from "./WaveSurferComponent.js";
+
+import Box from "@material-ui/core/Box";
+
+import "./styles.css";
+import styled from "styled-components";
+
+const StyledBox = styled(Box)`
+  #waveform {
+    margin: 24px auto;
+  }
+`;
+
+export default function App() {
+  const { audioURL } = useContext(MainContext);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>Audio Recorder</h2>
+      <AudioRecorderFunc />
+      {/* <AudioRecorder /> */}
+      {/* <SimpleRecorder /> */}
+      <StyledBox>
+        <WaveSurferComponent audioURL={audioURL} />
+        <button
+          onClick={() => {
+            console.log();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Log
+        </button>
+      </StyledBox>{" "}
     </div>
   );
 }
-
-export default App;
